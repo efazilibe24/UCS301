41b19e9a83b7043f8669ce0aa246f50d593b32c6
efazilibe24 <efazili_be24@thapar.edu>
2025-08-15 12:29:29 +0530

    Initial commit with assignments

---
+++ Assignment1/Q1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is Assignment 1 - Question 1\n";
+    return 0;
+}
---
+++ Assignment2/Q1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "This is Assignment 2 - Question 1\n";
+    return 0;
+}
