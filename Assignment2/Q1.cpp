008f1b1c71d97e651af2c915aaf53647d1e97e66
efazilibe24 <efazili_be24@thapar.edu>
2025-08-16 16:27:35 +0530

    q1 q2 done from assignment 2

--- Assignment2/Q1.cpp
+++ Assignment2/Q1.cpp
@@ -3,7 +3,50 @@ using namespace std;
 
 // BINARY SEARCH ALGORITHM //
 
+int arr[4] = { 1, 2, 3, 4}; // sorted array
+int arr_size = 4;
+
 int main() {
-    cout << "This is Assignment 2 - Question 1\n";
+    
+    int target;
+    cout << "Enter the number you want to search :";
+    cin >> target;
+    cout << endl;
+
+    int a = 0;
+    int b = arr_size - 1;
+
+    int m = ( a + b ) / 2;
+
+
+    while (a <= b)
+    {
+
+        if (target > arr[arr_size - 1] || target < arr[0])
+        {
+            cout << "Target not present in array" << endl;
+            break;
+        }
+
+
+        if (target > arr[m])
+        {
+            a = m + 1;
+        }
+        else if (target < arr[m])
+        {
+            b = m - 1;
+        }
+        else if (target == arr[m])
+        {
+            cout << "Target found at index: " << m << endl;
+            break;
+        }
+
+        m = ( a + b ) / 2 ;
+    }
+
+
+
     return 0;
 }
--- Assignment2/Q2.cpp
+++ Assignment2/Q2.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
 using namespace std;
 
+void displayArray();
+
 // BUBBLE SORT //
+
+
+int arr[7] = { 64, 34, 25, 12, 22, 11, 90 };
+int arr_size = 7;
+int no_of_it = 0;
 
 int main()
 {
+    for (int i = 0; i < arr_size; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < arr_size -1; j++)
+        {
+            if (arr[j] > arr[j+1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                swapped = true;
+            }
+        }
 
+        if (swapped == false)
+        {
+            break;
+        }
+
+        no_of_it++;
+    }
+
+    displayArray();
+
+
+}
+
+
+void displayArray()
+{
+    cout << "The array elements are : [ ";
+    for (int i =0; i < arr_size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+
+    cout << "]" << endl;
+
+    cout << "No. of iterations : " << no_of_it << endl;
 }
