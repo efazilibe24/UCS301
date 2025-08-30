174cda0c80dfb0e3ce419062a99051be1f4545be
efazilibe24 <efazili_be24@thapar.edu>
2025-08-30 16:08:11 +0530

    assignment 3

--- Assignment3/Q3.cpp
+++ Assignment3/Q3.cpp
@@ -1,12 +1,80 @@
 #include <iostream>
 using namespace std;
 
+#include <string>
+#include <stack>
+
 /* Write a program that checks if an expression has balanced parentheses.
 */
 
+bool isBalanced(string expr)
+{
+    stack<char> s;
+
+
+    for (char c : expr)
+    {
+        if (c == '{' || c == '[' || c == '(')
+        {
+            
+            s.push(c);
+        }
+        else if (c == '}' || c == ']' || c == ')')
+        {
+            
+            if (c == '}' && s.top() != '{')
+            {
+                
+                return false;
+            }
+            else if (c == ')' && s.top() != '(')
+            {
+                
+                return false;
+            }
+            else if (c == ']' && s.top() != '[')
+            {
+                
+                return false;
+            }
+
+
+            s.pop();
+
+        }
+    }
+
+
+    if (s.empty() == true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
 int main()
 {
+    string expr = "{ Hello [ () ] }";
 
+    
+    if (isBalanced(expr))
+    {
+        cout << "It is balanced" << endl;
+    }
+    else
+    {
+        cout << "It is not balanced" << endl;
+    }
 
 
     return 0;
